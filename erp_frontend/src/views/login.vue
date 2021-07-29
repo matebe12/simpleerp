da06ad31af6b16ba0de0f57dbd39f5390a5b284e
matebe12 <matebe12@gmail.com>
2021-07-30 04:18:27 +0900

    login rule add

--- erp_frontend/src/views/login.vue
+++ erp_frontend/src/views/login.vue
@@ -14,52 +14,66 @@
         >
             <v-container>
                 <v-layout justify-center align-center>
-                    <v-row>
-                        <v-col>
-                            <v-card class="mx-auto" max-width="400">
-                                <v-toolbar
-                                    color="#2C2E3E"
-                                    dark
-                                    class="window-header"
-                                    >로그인</v-toolbar
-                                >
-                                <v-form ref="form">
-                                    <v-container>
-                                        <v-col>
-                                            <v-text-field
-                                                label="아이디"
-                                                required
-                                                outlined
-                                                v-model="userId"
-                                                hide-details
-                                            ></v-text-field>
-                                        </v-col>
-                                        <v-col>
-                                            <v-text-field
-                                                label="비밀번호"
-                                                required
-                                                outlined
-                                                v-model="userPw"
-                                                append-icon="mdi-eye"
-                                                type="password"
-                                                hide-details
-                                            ></v-text-field>
-                                        </v-col>
-                                        <v-row>
+                    <form @submit.prevent="" form>
+                        <v-row>
+                            <v-col>
+                                <v-card class="mx-auto" max-width="400">
+                                    <v-toolbar
+                                        color="#2C2E3E"
+                                        dark
+                                        class="window-header"
+                                        >로그인</v-toolbar
+                                    >
+                                    <v-form ref="form">
+                                        <v-container>
                                             <v-col>
-                                                <v-btn
-                                                    width="100%"
-                                                    dark
-                                                    color="#2C2E3E"
-                                                    >로그인</v-btn
-                                                >
+                                                <v-text-field
+                                                    label="아이디"
+                                                    required
+                                                    outlined
+                                                    v-model="userId"
+                                                    hide-details
+                                                ></v-text-field>
                                             </v-col>
-                                        </v-row>
-                                    </v-container>
-                                </v-form>
-                            </v-card>
-                        </v-col>
-                    </v-row>
+                                            <v-col>
+                                                <v-text-field
+                                                    label="비밀번호"
+                                                    required
+                                                    outlined
+                                                    v-model="userPw"
+                                                    :append-icon="
+                                                        show
+                                                            ? 'mdi-eye'
+                                                            : 'mdi-eye-off'
+                                                    "
+                                                    :type="
+                                                        show
+                                                            ? 'text'
+                                                            : 'password'
+                                                    "
+                                                    @mousedown="show = !show"
+                                                    :rules="[
+                                                        rules.required,
+                                                        rules.min,
+                                                    ]"
+                                                ></v-text-field>
+                                            </v-col>
+                                            <v-row>
+                                                <v-col>
+                                                    <v-btn
+                                                        width="100%"
+                                                        dark
+                                                        color="#2C2E3E"
+                                                        >로그인</v-btn
+                                                    >
+                                                </v-col>
+                                            </v-row>
+                                        </v-container>
+                                    </v-form>
+                                </v-card>
+                            </v-col>
+                        </v-row>
+                    </form>
                 </v-layout>
             </v-container>
         </v-container>
@@ -72,7 +86,16 @@ import { Component, Vue } from 'vue-property-decorator';
     name: 'login',
     components: {},
 })
-export default class Employee extends Vue {}
+export default class Employee extends Vue {
+    userId = '';
+    userPw = '';
+    show = true;
+    rules = {
+        required: (value: any) => !!value || 'Required.',
+        min: (v: string | any[]) => v.length >= 8 || 'Min 8 characters',
+        emailMatch: () => `The email and password you entered don't match`,
+    };
+}
 </script>
 
 <style scoped>
